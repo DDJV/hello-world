f375770c56962682ab27583dd31490da492f4926
DUSTIN DAVID JARAMILLO VALENTIN <dustin_jaramillo@hotmail.com>
2017-02-15 07:35:27 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function ordenacion(a,b){
+    return a-b;
+}
+var arreglo=[6,5,3,1,8,7,2,4];
+arreglo.sort(ordenacion);
+console.log(arreglo);
+
+
+
+
