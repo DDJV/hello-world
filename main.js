ff05fa63d4e9da6bb56e589017493800a0f25693
DUSTIN DAVID JARAMILLO VALENTIN <dustin_jaramillo@hotmail.com>
2017-02-15 07:40:35 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Para ordenar los números utilizé la función sort para ordenarlos 
+ * de forma ascendente y
+ * ejecutarlo en modo consola.
+ * 
+ 
  */
 function ordenacion(a,b){
     return a-b;
+    /*
+     * Si a > b retorna 1
+     * Si b > a retorna -1
+     * Si b==a retorna 0
+     */
 }
 var arreglo=[6,5,3,1,8,7,2,4];
 arreglo.sort(ordenacion);
